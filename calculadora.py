2581dc73f0aad9abe9db4804dc06eca286f12ba1
davidfv21 <davidfv21@hotmail.com>
2020-05-21 16:51:24 -0500

    prueba

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+class calculadora ():
+     def _init_ (self):
+         self.valor1=3
+         self.valor2=5
+         self.resultado=0
+
+calculadora.sumar()
+print calculadora.resultado
+         
